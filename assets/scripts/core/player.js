855cca3f3c5ae54bee0fb88900b377b686c8247f
BaritoniumGMD <baritoniumgmd@gmail.com>
2026-09-11 22:55:25 -0500

    NOCLIP SLOPES YIPS

--- assets/scripts/core/player.js
+++ assets/scripts/core/player.js
@@ -4823,6 +4823,10 @@ if (this.p.isFlying || this.p.isUfo) {
             gameObj, pieceWidth, playersY, playersLastY, left, right, top, bottom, playerSize, waveHitSize, gamemodeAddition
           );
           if (slopeResult.died) {
+            if (window.noClip) {
+              this.p.diedThisFrame = true;
+              continue;
+            }
             this.killPlayer();
             return;
           }
